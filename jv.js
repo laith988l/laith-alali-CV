141fbf933018cf789b64d29ded88ee7bf36e59d2
laith988l <93006849+laith988l@users.noreply.github.com>
2025-08-23 21:14:12 +0200

    Add files via upload

---
+++ jv.js
@@ -0,0 +1,456 @@
+/*          *     .        *  .    *    *   . 
+ .  *  move your mouse to over the stars   .
+ *  .  .   change these values:   .  *
+   .      * .        .          * .       */
+const STAR_COLOR = '#fff';
+const STAR_SIZE = 3;
+const STAR_MIN_SCALE = 0.2;
+const OVERFLOW_THRESHOLD = 50;
+const STAR_COUNT = ( window.innerWidth + window.innerHeight ) / 8;
+
+const canvas = document.querySelector( 'canvas' ),
+      context = canvas.getContext( '2d' );
+
+let scale = 1, // device pixel ratio
+    width,
+    height;
+
+let stars = [];
+
+let pointerX,
+    pointerY;
+
+let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+
+let touchInput = false;
+
+generate();
+resize();
+step();
+
+window.onresize = resize;
+canvas.onmousemove = onMouseMove;
+canvas.ontouchmove = onTouchMove;
+canvas.ontouchend = onMouseLeave;
+document.onmouseleave = onMouseLeave;
+
+function generate() {
+
+   for( let i = 0; i < STAR_COUNT; i++ ) {
+    stars.push({
+      x: 0,
+      y: 0,
+      z: STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE )
+    });
+   }
+
+}
+
+function placeStar( star ) {
+
+  star.x = Math.random() * width;
+  star.y = Math.random() * height;
+
+}
+
+function recycleStar( star ) {
+
+  let direction = 'z';
+
+  let vx = Math.abs( velocity.x ),
+	    vy = Math.abs( velocity.y );
+
+  if( vx > 1 || vy > 1 ) {
+    let axis;
+
+    if( vx > vy ) {
+      axis = Math.random() < vx / ( vx + vy ) ? 'h' : 'v';
+    }
+    else {
+      axis = Math.random() < vy / ( vx + vy ) ? 'v' : 'h';
+    }
+
+    if( axis === 'h' ) {
+      direction = velocity.x > 0 ? 'l' : 'r';
+    }
+    else {
+      direction = velocity.y > 0 ? 't' : 'b';
+    }
+  }
+  
+  star.z = STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE );
+
+  if( direction === 'z' ) {
+    star.z = 0.1;
+    star.x = Math.random() * width;
+    star.y = Math.random() * height;
+  }
+  else if( direction === 'l' ) {
+    star.x = -OVERFLOW_THRESHOLD;
+    star.y = height * Math.random();
+  }
+  else if( direction === 'r' ) {
+    star.x = width + OVERFLOW_THRESHOLD;
+    star.y = height * Math.random();
+  }
+  else if( direction === 't' ) {
+    star.x = width * Math.random();
+    star.y = -OVERFLOW_THRESHOLD;
+  }
+  else if( direction === 'b' ) {
+    star.x = width * Math.random();
+    star.y = height + OVERFLOW_THRESHOLD;
+  }
+
+}
+
+function resize() {
+
+  scale = window.devicePixelRatio || 1;
+
+  width = window.innerWidth * scale;
+  height = window.innerHeight * scale;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  stars.forEach( placeStar );
+
+}
+
+function step() {
+
+  context.clearRect( 0, 0, width, height );
+
+  update();
+  render();
+
+  requestAnimationFrame( step );
+
+}
+
+function update() {
+
+  velocity.tx *= 0.96;
+  velocity.ty *= 0.96;
+
+  velocity.x += ( velocity.tx - velocity.x ) * 0.8;
+  velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+
+  stars.forEach( ( star ) => {
+
+    star.x += velocity.x * star.z;
+    star.y += velocity.y * star.z;
+
+    star.x += ( star.x - width/2 ) * velocity.z * star.z;
+    star.y += ( star.y - height/2 ) * velocity.z * star.z;
+    star.z += velocity.z;
+  
+    // recycle when out of bounds
+    if( star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD ) {
+      recycleStar( star );
+    }
+
+  } );
+
+}
+
+function render() {
+
+  stars.forEach( ( star ) => {
+
+    context.beginPath();
+    context.lineCap = 'round';
+    context.lineWidth = STAR_SIZE * star.z * scale;
+    context.globalAlpha = 0.5 + 0.5*Math.random();
+    context.strokeStyle = STAR_COLOR;
+
+    context.beginPath();
+    context.moveTo( star.x, star.y );
+
+    var tailX = velocity.x * 2,
+        tailY = velocity.y * 2;
+
+    // stroke() wont work on an invisible line
+    if( Math.abs( tailX ) < 0.1 ) tailX = 0.5;
+    if( Math.abs( tailY ) < 0.1 ) tailY = 0.5;
+
+    context.lineTo( star.x + tailX, star.y + tailY );
+
+    context.stroke();
+
+  } );
+
+}
+
+function movePointer( x, y ) {
+
+  if( typeof pointerX === 'number' && typeof pointerY === 'number' ) {
+
+    let ox = x - pointerX,
+        oy = y - pointerY;
+
+    velocity.tx = velocity.tx + ( ox / 8*scale ) * ( touchInput ? 1 : -1 );
+    velocity.ty = velocity.ty + ( oy / 8*scale ) * ( touchInput ? 1 : -1 );
+
+  }
+
+  pointerX = x;
+  pointerY = y;
+
+}
+
+function onMouseMove( event ) {
+
+  touchInput = false;
+
+  movePointer( event.clientX, event.clientY );
+
+}
+
+function onTouchMove( event ) {
+
+  touchInput = true;
+
+  movePointer( event.touches[0].clientX, event.touches[0].clientY, true );
+
+  event.preventDefault();
+
+}
+
+function onMouseLeave() {
+
+  pointerX = null;
+  pointerY = null;
+
+}
+
+
+
+var stats = new Stats();
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+
+
+/*!
+ * Mantis.js / jQuery / Zepto.js plugin for Constellation
+ * @version 1.2.2
+ * @author Acauã Montiel <contato@acauamontiel.com.br>
+ * @license http://acaua.mit-license.org/
+ */
+(function ($, window) {
+	var $window = $(window);
+	/**
+	 * Makes a nice constellation on canvas
+	 * @constructor Constellation
+	 */
+	function Constellation (canvas, options) {
+		var $canvas = $(canvas),
+			context = canvas.getContext('2d'),
+			defaults = {
+				star: {
+					color: 'rgba(255, 255, 255, .5)',
+					width: 1,
+					randomWidth: true
+				},
+				line: {
+					color: 'rgba(255, 255, 255, .5)',
+					width: 0.2
+				},
+				position: {
+					x: 0,
+					y: 0
+				},
+				width: window.innerWidth,
+				height: window.innerHeight,
+				velocity: 0.1,
+				length: 100,
+				distance: 120,
+				radius: 150,
+				stars: []
+			},
+			config = $.extend(true, {}, defaults, options);
+
+		function Star () {
+			this.x = Math.random() * canvas.width;
+			this.y = Math.random() * canvas.height;
+
+			this.vx = (config.velocity - (Math.random() * 0.5));
+			this.vy = (config.velocity - (Math.random() * 0.5));
+
+			this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
+		}
+
+		Star.prototype = {
+			create: function(){
+				context.beginPath();
+				context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+				context.fill();
+			},
+
+			animate: function(){
+				var i;
+				for (i = 0; i < config.length; i++) {
+
+					var star = config.stars[i];
+
+					if (star.y < 0 || star.y > canvas.height) {
+						star.vx = star.vx;
+						star.vy = - star.vy;
+					} else if (star.x < 0 || star.x > canvas.width) {
+						star.vx = - star.vx;
+						star.vy = star.vy;
+					}
+
+					star.x += star.vx;
+					star.y += star.vy;
+				}
+			},
+
+			line: function(){
+				var length = config.length,
+					iStar,
+					jStar,
+					i,
+					j;
+
+				for (i = 0; i < length; i++) {
+					for (j = 0; j < length; j++) {
+						iStar = config.stars[i];
+						jStar = config.stars[j];
+
+						if (
+							(iStar.x - jStar.x) < config.distance &&
+							(iStar.y - jStar.y) < config.distance &&
+							(iStar.x - jStar.x) > - config.distance &&
+							(iStar.y - jStar.y) > - config.distance
+						) {
+							if (
+								(iStar.x - config.position.x) < config.radius &&
+								(iStar.y - config.position.y) < config.radius &&
+								(iStar.x - config.position.x) > - config.radius &&
+								(iStar.y - config.position.y) > - config.radius
+							) {
+								context.beginPath();
+								context.moveTo(iStar.x, iStar.y);
+								context.lineTo(jStar.x, jStar.y);
+								context.stroke();
+								context.closePath();
+							}
+						}
+					}
+				}
+			}
+		};
+
+		this.createStars = function () {
+			var length = config.length,
+				star,
+				i;
+
+			context.clearRect(0, 0, canvas.width, canvas.height);
+
+			for (i = 0; i < length; i++) {
+				config.stars.push(new Star());
+				star = config.stars[i];
+
+				star.create();
+			}
+
+			star.line();
+			star.animate();
+		};
+
+		this.setCanvas = function () {
+			canvas.width = config.width;
+			canvas.height = config.height;
+		};
+
+		this.setContext = function () {
+			context.fillStyle = config.star.color;
+			context.strokeStyle = config.line.color;
+			context.lineWidth = config.line.width;
+		};
+
+		this.setInitialPosition = function () {
+			if (!options || !options.hasOwnProperty('position')) {
+				config.position = {
+					x: canvas.width * 0.5,
+					y: canvas.height * 0.5
+				};
+			}
+		};
+
+		this.loop = function (callback) {
+			callback();
+
+			this.rAF = window.requestAnimationFrame(function () {
+				stats.begin();
+				this.loop(callback);
+				stats.end();
+			}.bind(this));
+		};
+
+		this.handlers = {
+			window: {
+				mousemove: function(e){
+					config.position.x = e.pageX - $canvas.offset().left;
+					config.position.y = e.pageY - $canvas.offset().top;
+				},
+				resize: function () {
+					window.cancelAnimationFrame(this.rAF);
+				}
+			}
+		};
+
+		this.bind = function () {
+			$window
+				.on('mousemove', this.handlers.window.mousemove)
+				.on('resize', this.handlers.window.resize.bind(this));
+		};
+
+		this.unbind = function () {
+			$window
+				.off('mousemove', this.handlers.window.mousemove)
+				.off('resize', this.handlers.window.resize);
+		}
+
+		this.init = function () {
+			this.setCanvas();
+			this.setContext();
+			this.setInitialPosition();
+			this.loop(this.createStars);
+			this.bind();
+		};
+	}
+
+	function instantiate(element, options) {
+		var c = new Constellation(element, options);
+		c.init();
+	}
+
+	$.fn.constellation = function (options) {
+		return this.each(function () {
+			$window.on('resize', () => {
+				instantiate(this, options);
+			});
+
+			instantiate(this, options);
+		});
+	};
+})($, window);
+
+// Init plugin
+$('canvas').constellation({
+	star: {
+		width: 3
+	},
+	line: {
+		color: 'rgba(255, 255, 255, .5)'
+	},
+	length: (window.innerWidth / 6),
+	radius: (window.innerWidth / 5)
+});
